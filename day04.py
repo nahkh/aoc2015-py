b051a0e75f43f7b248fdea7fe1e061d3f63530ae
Juuso Valli <juuso.valli@gmail.com>
2024-06-15 20:38:16 +0200

    Day 4, part 2

--- day04.py
+++ day04.py
@@ -1,25 +1,31 @@
 from hashlib import md5
 
 
-def find_smallest_number_with_five_leading_zeroes(secret: str) -> int:
+def find_smallest_number_with_n_leading_zeroes(n: int, secret: str) -> int:
+    prefix = '0' * n
     value = 0
     while True:
         test_source = secret + str(value)
         hash_output = md5(test_source.encode())
         hash_string = hash_output.hexdigest()
-        if hash_string.startswith('00000'):
+        if hash_string.startswith(prefix):
             return value
         value += 1
 
 
 def part1():
-    number = find_smallest_number_with_five_leading_zeroes('iwrupvqb')
+    number = find_smallest_number_with_n_leading_zeroes(5, 'iwrupvqb')
     print(f'Day 4, part 1: The smallest number that has a hash starting with 5 zeroes is {number}')
 
+
+def part2():
+    number = find_smallest_number_with_n_leading_zeroes(6, 'iwrupvqb')
+    print(f'Day 4, part 2: The smallest number that has a hash starting with 6 zeroes is {number}')
 
 
 def main():
     part1()
+    part2()
 
 
 if __name__ == '__main__':
