5542b28bacd059160f92114b3684257edbb2496d
Juuso Valli <juuso.valli@gmail.com>
2024-06-22 07:52:21 +0200

    Day 11, part 1

--- day11.py
+++ day11.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from functools import cache
 
 
 def next_candidate(password: str) -> str:
@@ -13,5 +14,56 @@ def next_candidate(password: str) -> str:
     return prefix + chr(next_last_char)
 
 
+@cache
+def get_straights():
+    output = []
+    for start in range(ord('a'), ord('y')):
+        output.append(chr(start) + chr(start + 1) + chr(start + 2))
+    return tuple(output)
+
+
+def password_contains_increasing_straight(password: str) -> bool:
+    for straight in get_straights():
+        if straight in password:
+            return True
+    return False
+
+
+def password_contains_two_pairs(password: str) -> bool:
+    pair_count = 0
+    prev_char = None
+    for c in password:
+        if c == prev_char:
+            pair_count += 1
+            prev_char = None
+            continue
+        prev_char = c
+    return pair_count >= 2
+
+
+def password_does_not_contain_banned_characters(password: str) -> bool:
+    return 'i' not in password and 'o' not in password and 'l' not in password
+
+
 def password_is_valid(password: str) -> bool:
-    pass
+    return password_contains_increasing_straight(password) and password_does_not_contain_banned_characters(
+        password) and password_contains_two_pairs(password)
+
+
+def find_next_valid_password(password: str) -> str:
+    candidate = next_candidate(password)
+    while not password_is_valid(candidate):
+        candidate = next_candidate(candidate)
+    return candidate
+
+
+def part1():
+    print(f'Day 11, part 1: Santa\'s next password will be {find_next_valid_password("cqjxjnds")}')
+
+
+def main():
+    part1()
+
+
+if __name__ == '__main__':
+    main()
--- day11_test.py
+++ day11_test.py
@@ -15,6 +15,16 @@ class TestDay11(unittest.TestCase):
         self.assertEqual(password_is_valid('abbceffg'), False)
         self.assertEqual(password_is_valid('abbcegjk'), False)
 
+    def test_straights(self):
+        straights = get_straights()
+        self.assertIn('abc', straights)
+        self.assertIn('xyz', straights)
+        self.assertEqual(len(straights), 24)
+
+    def test_find_next_valid_password(self):
+        self.assertEqual(find_next_valid_password('abcdefgh'), 'abcdffaa')
+        self.assertEqual(find_next_valid_password('ghijklmn'), 'ghjaabcc')
+
 
 if __name__ == '__main__':
     unittest.main()
