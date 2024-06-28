bd1eca63038c9f964350fba3d9ad64341b967d38
Juuso Valli <juuso.valli@gmail.com>
2024-06-28 18:43:37 +0200

    Day 17, part 2

--- day17.py
+++ day17.py
@@ -20,7 +20,58 @@ def count_possible_ways_to_reach_sum(target: int, values: Tuple[int]) -> int:
 
 
 def part1(numbers: tuple[int]):
-    print(f'The number of ways to transfer 150 gallons is {count_possible_ways_to_reach_sum(150, numbers)}')
+    print(
+        f'Day 17, part 1: The number of ways to transfer 150 gallons is {count_possible_ways_to_reach_sum(150, numbers)}')
+
+
+@cache
+def find_minimum_number_of_containers(target: int, values: Tuple[int], used_containers: int) -> int:
+    if target == 0:
+        return used_containers
+    if len(values) == 0:
+        return 0
+    next_values = tuple(list(values)[1:])
+    candidates = []
+    if values[0] == target:
+        return used_containers + 1
+    elif values[0] < target:
+        candidates.append(find_minimum_number_of_containers(target - values[0], next_values, used_containers + 1))
+    candidates.append(find_minimum_number_of_containers(target, next_values, used_containers))
+    nonzero_candidates = [candidate for candidate in candidates if candidate > 0]
+    if nonzero_candidates:
+        return min(nonzero_candidates)
+    else:
+        return 0
+
+
+@cache
+def count_possible_ways_to_reach_sum_with_n_containers(target: int, values: Tuple[int],
+                                                       containers_remaining: int) -> int:
+    if containers_remaining == 0:
+        if target == 0:
+            return 1
+        else:
+            return 0
+    if containers_remaining == 1:
+        return sum(1 for value in values if value == target)
+    if len(values) == 0:
+        return 0
+    possible_ways = 0
+    next_values = tuple(list(values)[1:])
+    if values[0] == target and containers_remaining == 1:
+        possible_ways += 1
+    elif values[0] < target and containers_remaining > 1:
+        possible_ways += count_possible_ways_to_reach_sum_with_n_containers(target - values[0], next_values,
+                                                                            containers_remaining - 1)
+    possible_ways += count_possible_ways_to_reach_sum_with_n_containers(target, next_values, containers_remaining)
+    return possible_ways
+
+
+def part2(numbers: tuple[int]):
+    minimum_number_of_containers = find_minimum_number_of_containers(150, numbers, 0)
+    number_of_ways = count_possible_ways_to_reach_sum_with_n_containers(150, numbers, minimum_number_of_containers)
+    print(
+        f'Day 17, part 2: The number of ways to transfer 150 gallons while using {minimum_number_of_containers} containers is {number_of_ways}')
 
 
 def main():
@@ -28,6 +79,7 @@ def main():
         numbers = list(map(int, f.readlines()))
         numbers.sort(reverse=True)
         part1(tuple(numbers))
+        part2(tuple(numbers))
 
 
 if __name__ == '__main__':
--- day17_test.py
+++ day17_test.py
@@ -5,7 +5,13 @@ from day17 import *
 class TestDay17(unittest.TestCase):
 
     def test_count_possible_ways_to_reach_sum(self):
-        self.assertEqual(count_possible_ways_to_reach_sum(25, (20, 15, 10, 5, 5)), 4)
+        self.assertEqual(4, count_possible_ways_to_reach_sum(25, (20, 15, 10, 5, 5)))
+
+    def test_find_minimum_number_of_containers(self):
+        self.assertEqual(2, find_minimum_number_of_containers(25, (20, 15, 10, 5, 5), 0))
+
+    def test_count_possible_ways_to_reach_sum_with_n_containers(self):
+        self.assertEqual(3, count_possible_ways_to_reach_sum_with_n_containers(25, (20, 15, 10, 5, 5), 2))
 
 
 if __name__ == '__main__':
