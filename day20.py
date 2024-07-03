f16a4c1360c25568faf76ea78305901ef1b688d2
Juuso Valli <juuso.valli@gmail.com>
2024-07-03 12:27:50 +0200

    Day 20, part 2

--- day20.py
+++ day20.py
@@ -1,83 +1,47 @@
 from __future__ import annotations
-from typing import Generator, Tuple, Dict
-import dataclasses
-import math
-from functools import reduce
+from typing import Tuple, Optional
 
 
-@dataclasses.dataclass
-class PrimeCache:
-    primes: tuple[int]
+def get_first_house_with_n_presents(presents: int, presents_per_visit: int, max_visits: int = None) -> Tuple[int, int]:
+    def depth_limited_search(depth: int) -> Optional[Tuple[int, int]]:
+        present_counts = [presents_per_visit] * (depth + 1)
+        for elf in range(2, depth + 1):
+            index = elf
+            present_counts[index] += presents_per_visit * elf
+            if present_counts[elf] >= presents:
+                return elf, present_counts[elf]
+            visits = 1
+            index += elf
+            while index < len(present_counts) and (not max_visits or visits < max_visits):
+                present_counts[index] += presents_per_visit * elf
+                index += elf
+                visits += 1
+        for house in range(1, len(present_counts)):
+            if present_counts[house] >= presents:
+                return house, present_counts[house]
+        return None
 
-    @classmethod
-    def create(cls, max_prime: int):
-        sieve = [True] * (max_prime + 2)
-        for i in range(2, math.ceil(math.sqrt(max_prime)) + 1):
-            j = 2
-            while i * j < len(sieve):
-                sieve[i*j] = False
-                j += 1
-        primes = []
-        for i in range(2, len(sieve)):
-            if sieve[i]:
-                primes.append(i)
-        return PrimeCache(tuple(primes))
-
-    def factorize(self, n: int) -> Dict[int, int]:
-        results = {}
-        for prime in self.primes:
-            while n % prime == 0:
-                results[prime] = results.get(prime, 0) + 1
-                n = n // prime
-            if n == 1:
-                return results
-        assert False, f'We should not have reached here: {n} remains'
-
-    def get_divisors(self, n: int) -> Generator[int]:
-        if n == 1:
-            yield 1
-            return
-        factors = [x for x in self.factorize(n).items()]
-        nfactors = len(factors)
-        f = [0] * nfactors
-        while True:
-            yield reduce(lambda x, y: x*y, [factors[x][0]**f[x] for x in range(nfactors)], 1)
-            i = 0
-            while True:
-                f[i] += 1
-                if f[i] <= factors[i][1]:
-                    break
-                f[i] = 0
-                i += 1
-                if i >= nfactors:
-                    return
-
-    def sum_of_divisors(self, n: int) -> int:
-        result = 1
-        for prime, m in self.factorize(n).items():
-            result *= (prime ** (m+1) - 1) // (prime - 1)
-        return result
-
-
-def get_number_of_presents(prime_cache: PrimeCache, house_number: int) -> int:
-    return 10 * prime_cache.sum_of_divisors(house_number)
+    current_depth = 1000000
+    result = None
+    while not result:
+        result = depth_limited_search(current_depth)
+        current_depth *= 10
+    return result
 
 
 def part1(min_presents: int):
-    primes = PrimeCache.create(min_presents // 10)
-    best_presents = 0
-    current_house = 0
-    while best_presents < min_presents:
-        current_house += 1
-        presents = get_number_of_presents(primes, current_house)
-        if presents > best_presents:
-            best_presents = presents
+    house, presents = get_first_house_with_n_presents(min_presents, 10)
+    print(f'Day 20, part 1: House {house} got {presents}')
 
-    print(f'Day 20, part 1: House {current_house} got {best_presents}')
+
+def part2(min_presents: int):
+    house, presents = get_first_house_with_n_presents(min_presents, 11, 50)
+    print(f'Day 20, part 2: House {house} got {presents}')
 
 
 def main():
     part1(33100000)
+    part2(33100000)
 
 
 if __name__ == '__main__':
